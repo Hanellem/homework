3f83a0b75eb5c2f20652af2a99337d4e406e2c73
Your Name <magala.viktor67@gmail.com>
2022-02-06 22:20:11 +0200

    file

---
+++ view_edit_password.php
@@ -0,0 +1,46 @@
+<?php
+
+session_start();
+$name = $_SESSION['name'];
+$email = $_SESSION['email'];
+$tel = $_SESSION['tel']; 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Open+Sans&family=Ubuntu:wght@400;700&display=swap" rel="stylesheet">
+	
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+	<section id="mail" class="mail">
+		<div class="container">
+		<form action="php/edit_password.php" method="POST" class="form">
+				<img src="image/avatar.jpg" alt="avatar">
+				
+				<p>NAME - <?= $name ?></p>
+				<p>EMAIL - <?= $email ?></p>
+				<p>TELEPHONE - <?= $tel ?></p>
+
+				
+				<input type="password" placeholder="Действующий пароль" name="password" class="input input-profile" required>
+                <input type="password" placeholder="Новый пароль" name="new_password" class="input input-profile" required>
+                <input type="password" placeholder="Повторите пароль" name="password_confirm" class="input input-profile" required>
+				
+
+
+				<button type="submit" class="btn btn-profile">Сохранить изменения</button>
+		</fprm>		
+		</div>
+	</section>
+			
+
+</body>
+</html>
